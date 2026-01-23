833b3920f143b07dafeff66654f0cb17602071cd
rouvjub38 <rouviere.julie@neuf.fr>
2026-01-23 13:42:26 +0100

    Update scriptmultiplication.js

--- docs/multiplications/scriptmultiplication.js
+++ docs/multiplications/scriptmultiplication.js
@@ -6,23 +6,35 @@ let score=0;
 
 for (let i=0 ; i<100; i++){
 
-var chiffre1=Math.floor(Math.random()*9+1),chiffre2=Math.floor(Math.random()*9+1);
-var calcul=chiffre1*chiffre2
+let chiffre1 = Math.floor(Math.random()*9+1);
+let chiffre2 = Math.floor(Math.random()*9+1);
+let calcul = chiffre1*chiffre2;
 
 let calculUtilisateur= prompt("combien font "+chiffre1 + " x " +chiffre2 + "?");
 
-
-    if (calcul==calculUtilisateur){
+//cas où l'utilisateur ne répond rien
+    if (calculUtilisateur===null||calculUtilisateur.trim()===""){
+        alert("tu n'as pas répondu");
+//cas bonne réponse
+    else if (Number(calculUtilisateur)===calcul){
         score+=1;
-        alert("la réponse est : "+ calcul + ", bravo "+Nomutilisateur +", ton scrore est de : " + score);
-       
+        alert("la réponse est : "+ calcul + 
+              ", bravo "+Nomutilisateur +
+              ", ton scrore est de : " + score);
     }
+//mauvaise réponse
     else{
         score+=0;
         alert("la response était : " +calcul +
-        " ce n'est pas la bonne réponse, " + Nomutilisateur + ", ton score est de : "+ score);
+            " ce n'est pas la bonne réponse, " + Nomutilisateur + 
+            ", ton score est de : "+ score);
 }
+//option pour arrêter le jeu
+    let continuer=confirm'"Veux-tu continuer?");
+    if(!continuer){
+        break;
 }
+    alert("Jeu terminé / score final :" + score);
 
 
 
