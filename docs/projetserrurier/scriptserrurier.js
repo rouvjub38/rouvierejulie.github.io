1df3f1e0e98bdce142971f0e27c3809a59040d35
rouvjub38 <rouviere.julie@neuf.fr>
2026-01-23 13:03:17 +0100

    Create scriptserrurier.js

---
+++ docs/projetserrurier/scriptserrurier.js
@@ -0,0 +1,10 @@
+document.getElementById("contatcForm").addEventListener("submit",function(e){
+  e.preventDefault();//empeche l'envoi reel
+
+  //afficher le message
+  document.getElementById("successMessage").slyle.display="block";
+
+  // vider le formulaire
+  this.reset();
+});
+                                                        
