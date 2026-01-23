3ac9cdcc90a0bd33a06999ba39308ad7ff75e90a
rouvjub38 <rouviere.julie@neuf.fr>
2026-01-23 14:01:06 +0100

    Update scriptmultiplication.js

--- docs/multiplications/scriptmultiplication.js
+++ docs/multiplications/scriptmultiplication.js
@@ -1,10 +1,13 @@
 let Nomutilisateur=prompt("Quel est votre Prénom?");
+
+if (Nomutilisateur===null || Nomutilisateur.trim()===""){
+    Nomutilisateur="Joueur";
+}
     alert("Bonjour "+Nomutilisateur);
 
 let score=0;
 
-
-for (let i=0 ; i<100; i++){
+for (let i=0 ; i<100; i++) {
 
 let chiffre1 = Math.floor(Math.random()*9+1);
 let chiffre2 = Math.floor(Math.random()*9+1);
@@ -12,29 +15,36 @@ let calcul = chiffre1*chiffre2;
 
 let calculUtilisateur= prompt("combien font "+chiffre1 + " x " +chiffre2 + "?");
 
-//cas où l'utilisateur ne répond rien
+
+//cas annuler ou vide
     if (calculUtilisateur===null||calculUtilisateur.trim()===""){
         alert("tu n'as pas répondu");
+    }
 //cas bonne réponse
     else if (Number(calculUtilisateur)===calcul){
-        score+=1;
-        alert("la réponse est : "+ calcul + 
-              ", bravo "+Nomutilisateur +
-              ", ton scrore est de : " + score);
+        score++;
+        alert(
+            "la réponse est : "+ calcul + 
+              ", \nBravo "+Nomutilisateur +
+              ", \nTon scrore est de : " + score
+            );
     }
 //mauvaise réponse
     else{
-        score+=0;
-        alert("la response était : " +calcul +
-            " ce n'est pas la bonne réponse, " + Nomutilisateur + 
-            ", ton score est de : "+ score);
+        alert(
+            "La response était : " +calcul +
+            " \nCe n'est pas la bonne réponse, " + Nomutilisateur + 
+            ", \nTon score est de : "+ score
+        );
 }
 //option pour arrêter le jeu
-    let continuer=confirm'"Veux-tu continuer?");
+    let continuer=confirm("Veux-tu continuer?");
     if(!continuer){
         break;
 }
-    alert("Jeu terminé / score final :" + score);
+}
+    alert("Jeu terminé \nScore final :" + score);
+
 
 
 
