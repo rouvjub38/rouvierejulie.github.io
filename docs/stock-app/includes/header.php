df7d8ab38c6bb0d410d7e90f1dd07be2f6af6468
rouvjub38 <rouviere.julie@neuf.fr>
2026-02-19 23:48:33 +0100

    Create header.php

---
+++ docs/stock-app/includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Gestion de stock</title>
+        <link rel="stylesheet" href="/stock-app/assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Gestion de stock</h1>
+        <hr>
+
